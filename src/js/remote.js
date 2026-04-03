e5acc50f68dcbb9428d5029d37056caff3921fe3
Jan <github@sintho.com>
2026-04-03 10:35:15 +0200

    replaced and upgrades dependencies

--- src/js/remote.js
+++ src/js/remote.js
@@ -1,4 +1,4 @@
-const { Keys } = require('../data/RemoteKeys');
+import { Keys } from "../data/RemoteKeys.json";
 
 /**
  * @description TVRemote class, controls the clicking of the buttons on the svg-remote
@@ -6,24 +6,24 @@ const { Keys } = require('../data/RemoteKeys');
  * @class TVRemote
  */
 class TVRemote {
-    /**
-     * @description Creates an instance of TVRemote.
-     *
-     * @example TVRemote();
-     * @author Jan Kaiser
-     * @memberof TVRemote
-     */
-    constructor() {
-        const svg = document.getElementById('remote-svg');
-        svg.addEventListener('load', () => {
-            const svgDoc = svg.contentDocument;
-            Keys.forEach((key) => {
-                svgDoc.getElementById(key).addEventListener('click', function (e) {
-                    fetch(`http://${__HOST__}:${__PORT__}/api/tv/${this.id}`);
-                });
-            });
+  /**
+   * @description Creates an instance of TVRemote.
+   *
+   * @example TVRemote();
+   * @author Jan Kaiser
+   * @memberof TVRemote
+   */
+  constructor() {
+    const svg = document.getElementById("remote-svg");
+    svg.addEventListener("load", () => {
+      const svgDoc = svg.contentDocument;
+      Keys.forEach((key) => {
+        svgDoc.getElementById(key).addEventListener("click", function () {
+          fetch(`/api/tv/${this.id}`);
         });
-    }
+      });
+    });
+  }
 }
 
 window.remote = new TVRemote();
